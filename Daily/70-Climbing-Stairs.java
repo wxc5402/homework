9bb3d4b3a390d994e0f25dc2e5823fef08a4ae5d
wxc5402 <90800968+wxc5402@users.noreply.github.com>
2021-09-26 21:02:58 -0400

    Create 70-Climbing-Stairs.java

---
+++ Daily/70-Climbing-Stairs.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int climbStairs(int n) {
+        int memo[] = new int[n + 1];
+        return climbStairsMemo(n, memo);
+    }
+
+    public int climbStairsMemo(int n ,  int[] memo) {
+        if (memo[n] > 0) {
+            return memo[n];
+        }
+
+        if ( n == 1) {
+            memo[n] = 1;
+        } else if ( n == 2) {
+            memo[n] = 2;
+        } else {
+            memo[n] = climbStairsMemo(n - 1, memo) + climbStairsMemo(n - 2, memo);
+        }
+        return memo[n];
+    }
+}
