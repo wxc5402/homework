228c9290f11d90a83266b3b9c05464cf8060ba78
wxc5402 <90800968+wxc5402@users.noreply.github.com>
2021-10-04 22:56:24 -0400

    Create 155-MinStack.java

---
+++ Daily/155-MinStack.java
@@ -0,0 +1,28 @@
+class MinStack {
+    Deque<Integer> xStack;
+    Deque<Integer> minStack;
+
+    public MinStack() {
+        xStack = new LinkedList<Integer>();
+        minStack = new LinkedList<Integer>();
+        minStack.push(Integer.MAX_VALUE);
+    }
+    
+    public void push(int val) {
+        xStack.push(val);
+        minStack.push(Math.min(minStack.peek(), val));
+    }
+    
+    public void pop() {
+        xStack.pop();
+        minStack.pop();
+    }
+    
+    public int top() {
+        return xStack.peek();
+    }
+    
+    public int getMin() {
+        return minStack.peek();
+    }
+}
