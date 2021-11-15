7f1ac4cc46bdeee1830a1a648d0683994133fc45
wxc5402 <90800968+wxc5402@users.noreply.github.com>
2021-11-15 10:03:50 -0500

    Create 704-Binary-Search.java

---
+++ Daily/704-Binary-Search.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int low = 0;
+        int high = nums.length - 1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            int num = nums[mid];
+
+            if (num == target) {
+                return mid;
+            } else if (num > target) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+
+        return -1;
+    }
+}
